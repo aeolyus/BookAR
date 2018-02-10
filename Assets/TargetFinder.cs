bbbf9c5a7e35a93890c304bb285b97ae6e7f6e91
Richard Huang <richardhuang.huang@gmail.com>
2018-02-10 14:20:49 -0800

    initial unity commit

---
+++ Assets/TargetFinder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetFinder : IUserDefinedTargetEventHandler {
+
+	// Use this for initialization
+	void OnInitialized() {
+		Debug.Log("initialization");
+	}
+	
+	// Update is called once per frame
+	void OnFrameQualityChanged(ImageTargetBuilder.FrameQuality frameQuality() {
+		Debug.Log("frame quality changed");
+	}
+
+	void OnNewTrackableSource(TrackableSource trackableSource){
+		Debug.Log("on new trackable source");
+	}
+}
---
+++ Assets/TargetStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetStart : MonoBehaviour {
+	TargetFinder targetFinder;
+	// Use this for initialization
+	void Start () {
+		targetFinder=new TargetFinder();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
